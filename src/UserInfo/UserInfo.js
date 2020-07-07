81c14810b819a49db9855176ba22280eb15d4f92
linlifu2 <linlin.fu@thoughtworks.com>
2020-07-07 22:40:55 +0800

    update user component

--- src/UserInfo/UserInfo.js
+++ src/UserInfo/UserInfo.js
@@ -3,7 +3,25 @@ import './UserInfo.scss';
 
 class UserInfo extends Component {
   render() {
-    return <aside className="UserInfo">User Info</aside>;
+    const { name, email, phone, website, company } = this.props.user;
+    return (
+      <aside className="UserInfo">
+        <h2>{name}</h2>
+        <div className="contactLabel"> Contact Me @</div>
+        <div className="userInfoList">
+          {Object.entries({ email, phone, website }).map(([key, value]) => (
+            <ul key={key}>
+              <div className="itemName">{key}:</div>
+              <div className="itemValue">{value}</div>
+            </ul>
+          ))}
+          <ul>
+            <div className="itemName">company:</div>
+            <div>{company?.name}</div>
+          </ul>
+        </div>
+      </aside>
+    );
   }
 }
 
